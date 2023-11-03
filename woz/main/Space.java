94e0379524bd51dec65a207a2e063ca1da6b6896
Lars Kristiansen <larkr23@student.sdu.dk>
2023-11-03 08:40:43 +0100

    change number to "." in space

--- woz/main/Space.java
+++ woz/main/Space.java
@@ -3,39 +3,39 @@ package main;
 import java.util.Set;
 
 class Space extends Node {
-  Space (String name) {
-    super(name);
-  }
-  
-  public void welcome () {
-
-      System.out.println("You are now walking to " + name);
-      System.out.println("You will be arriving in");
-
-      for (int i = 5; i > 0; i--) {
-        try {
-          Thread.sleep(1000);
-        } catch (InterruptedException e) {
-          throw new RuntimeException(e);
-        }
-        System.out.println(i);
-      }
-      System.out.println("You are now at "+name);
-
-    Set<String> exits = edges.keySet();
-    System.out.println("Current exits are:");
-    for (String exit: exits) {
-      System.out.println(" - "+exit);
+    Space(String name) {
+        super(name);
     }
-  }
+
+    public void welcome() {
+
+        System.out.println("You are now walking to " + name);
 
 
-  public void goodbye () {
-    System.out.println("You are leaving "+name);
-  }
-  
-  @Override
-  public Space followEdge (String direction) {
-    return (Space) (super.followEdge(direction));
-  }
+        for (int i = 5; i > 0; i--) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print(".");
+        }
+        System.out.println("\nYou are now at " + name);
+
+        Set<String> exits = edges.keySet();
+        System.out.println("Current exits are:");
+        for (String exit : exits) {
+            System.out.println(" - " + exit);
+        }
+    }
+
+
+    public void goodbye() {
+        System.out.println("You are leaving " + name);
+    }
+
+    @Override
+    public Space followEdge(String direction) {
+        return (Space) (super.followEdge(direction));
+    }
 }
